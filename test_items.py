d7f74eb06fb9636d559dd94d4dbef11fdeaef6ce
val <you@ex.com>
2022-07-25 21:55:37 +0300

    _with_sleep

--- test_items.py
+++ test_items.py
@@ -1,3 +1,4 @@
+from time import sleep
 from selenium.webdriver.common.by import By
 
 
@@ -6,4 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_find_cart_button(browser):
     browser.get(link)
     item = browser.find_elements(By.CSS_SELECTOR,"#add_to_basket_form button")
+    sleep(5)
     assert item, 'no item'
