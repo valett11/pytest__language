a861a9de2c9ac2334e56a450f70f49711373bcb3
val <you@ex.com>
2022-07-25 21:52:51 +0300

    test_ready

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_find_cart_button(browser):
+    browser.get(link)
+    item = browser.find_elements(By.CSS_SELECTOR,"#add_to_basket_form button")
+    assert item, 'no item'
